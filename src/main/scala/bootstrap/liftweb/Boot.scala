7a5a4f252be2f6fa778f1cd4a449ceaf66fe8159
Channing Walton <channingwalton@mac.com>
2016-01-20 22:32:52 +0000

    cleaned up a bit

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -15,9 +15,13 @@ import net.liftmodules.JQueryModule
 
 /**
   * A class that's instantiated early and run.  It allows the application
-  * to modify lift's environment
+  * to modify lift's environment.
+  *
+  *  You can make calls to LiftRules in boot(), after which LiftRules
+  *  cannot be modified.
   */
 class Boot {
+
   def boot(): Unit = {
 
     // where to search snippet
@@ -30,7 +34,7 @@ class Boot {
       // more complex because this menu allows anything in the
       // /static path to be visible
       Menu(Loc("Static", Link(List("static"), true, "/static/index"), "Static Content")))
-      
+
     LiftRules.setSiteMap(sitemap)
 
     //Init the jQuery module, see http://liftweb.net/jquery for more information.
@@ -38,19 +42,10 @@ class Boot {
     JQueryModule.InitParam.JQuery=JQueryModule.JQuery191
     JQueryModule.init()
 
-    //Show the spinny image when an Ajax call starts
-    //LiftRules.ajaxStart =
-    //  Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-
-    // Make the spinny image go away when it ends
-    //LiftRules.ajaxEnd =
-    //  Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
-
     // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
 
     // Use HTML5 for rendering
-    LiftRules.htmlProperties.default.set((r: Req) =>
-      new Html5Properties(r.userAgent))
+    LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))
   }
 }
