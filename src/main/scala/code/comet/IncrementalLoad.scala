2630b8dab3453ed9afc97cc7b2be4cf55e9b8fd8
Channing Walton <channing.walton@barclays.com>
2017-05-16 20:11:35 +0100

    no need to use a schedule

--- src/main/scala/code/comet/IncrementalLoad.scala
+++ src/main/scala/code/comet/IncrementalLoad.scala
@@ -1,50 +1,55 @@
 package code.comet
 
-import java.util.concurrent.ScheduledFuture
-
-import net.liftweb.http.js.jquery.JqJE.{JqAppend, JqId}
-import net.liftweb.http.js.{JsCmd, JsCmds}
-import net.liftweb.http.{CometActor, RenderOut, SHtml}
-import net.liftweb.util.Schedule
-
-import scala.xml.{NodeSeq, Text}
+import net.liftweb.http.js.JsCmd
 import net.liftweb.http.js.JsCmds._
 import net.liftweb.http.js.JsExp.strToJsExp
+import net.liftweb.http.js.jquery.JqJE.{JqAppend, JqId}
+import net.liftweb.http.{CometActor, RenderOut, SHtml}
+
+import scala.xml.NodeSeq
 
 class IncrementalLoad extends CometActor {
 
   case class Rows(rows: List[Int])
 
   // assume a big table of data
-  val bigData: Rows = Rows((0 until 1000).toList)
+  val bigData: Rows = Rows((0 until 100).toList)
 
+  // this method is called when the page first loads
   override def render: RenderOut = {
-    scheduleNextPush(bigData)
+    // send the data to this actor to get things going
+    this ! bigData
 
     // return the html that will have rows appended
     <div id="theTable">
     </div>
   }
 
-  private def scheduleNextPush(data: Rows): ScheduledFuture[Unit] = {
-    Schedule.perform(this, data, 1000)
-  }
+  // this method handles messages sent to the actor
+  override def lowPriority: PartialFunction[Any, Unit] = {
 
-  override def lowPriority : PartialFunction[Any, Unit] = {
     case Rows(data) ⇒ waitForBrowser {
       val (chunk, rest) = data.splitAt(10)
-      val html = chunk.map(i ⇒ <div>Row
-        {i}
-      </div>)
+      val html = chunk.map(i ⇒ <div>Row{i}</div>)
 
       // send the chunk
       partialUpdate(appendChild("theTable", html))
 
-      if (rest.nonEmpty) scheduleNextPush(Rows(rest))
+      if (rest.nonEmpty) this ! Rows(rest)
     }
   }
 
-  def appendChild(id: String, body: NodeSeq): JsCmd = JqId(id) ~> JqAppend(body)
+  // append some html as a child to an element
+  def appendChild(id: String, body: NodeSeq): JsCmd =
+    JqId(id) ~> JqAppend(body)
 
-  def waitForBrowser(f: ⇒ Unit): Unit = partialUpdate(SHtml.ajaxInvoke(() ⇒ f))
+  /* This takes a function which will be invoked after
+   * the browser calls back to the server.
+   *
+   * Doing this allows the server to be sure
+   * that the browser is free before pushing
+   * the next chunk
+   */
+  def waitForBrowser(f: ⇒ Unit): Unit =
+    partialUpdate(SHtml.ajaxInvoke(() ⇒ f))
 }
